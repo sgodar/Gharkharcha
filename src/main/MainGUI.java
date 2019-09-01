691300f1fae2ac0dee4f05a70a45912469d19598
sgodar <uniquefren16@gmail.com>
2019-09-01 09:14:02 +0545

    invalid

--- src/main/MainGUI.java
+++ src/main/MainGUI.java
@@ -12,197 +12,199 @@ import model.Expenditure;
 
 public class MainGUI extends JFrame implements ActionListener { // inheriting JFrame
 
- JLabel label;
- JButton button, btnRent, btnElectricity, btnExpenditure, btnTrent, btntExpenditure, btnExit;
- JTextField t;
- Database db;
- Expenditure exp = new Expenditure();
- GrandTotal gt = new GrandTotal();
- //verify v = new verify();
- int unitPrice = 15;
- MainGUI() {
-  db = new Database();
-  btnRent = new JButton("Rent"); // create button
-  btnRent.addActionListener(this);
-  btnElectricity = new JButton("Electricity");
-  btnElectricity.addActionListener(this);
-  btnExpenditure = new JButton("Daily Kharcha");
-  btnExpenditure.addActionListener(this);
-  btnTrent = new JButton("Rent amount");
-  btnTrent.addActionListener(this);
-  btntExpenditure = new JButton("Total Expenditure");
-  btntExpenditure.addActionListener(this);
-  btnExit = new JButton("Exit");
-  btnExit.addActionListener(this);
+	JLabel label;
+	JButton button, btnRent, btnElectricity, btnExpenditure, btnTrent, btntExpenditure, btnExit;
+	JTextField t;
+	Database db;
+	Expenditure exp = new Expenditure();
+	GrandTotal gt = new GrandTotal();
+	// verify v = new verify();
+	int unitPrice = 15;
 
-  JPanel p = new JPanel();
-  p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
-  p.add(btnRent);
-  p.add(btnElectricity);
-  p.add(btnExpenditure);
-  p.add(btnTrent);
-  p.add(btntExpenditure);
-  p.add(btnExit);
-  add(p);
-  setSize(400, 500);
-  setVisible(true);
- }
+	MainGUI() {
+		db = new Database();
+		btnRent = new JButton("Add Rent"); // create button
+		btnRent.addActionListener(this);
+		btnElectricity = new JButton("Add Electricity");
+		btnElectricity.addActionListener(this);
+		btnExpenditure = new JButton("Daily Kharcha");
+		btnExpenditure.addActionListener(this);
+		btnTrent = new JButton("Rent amount");
+		btnTrent.addActionListener(this);
+		btntExpenditure = new JButton("Total Expenditure");
+		btntExpenditure.addActionListener(this);
+		btnExit = new JButton("Exit");
+		btnExit.addActionListener(this);
 
- @Override
- public void actionPerformed(ActionEvent e) {
+		JPanel p = new JPanel();
+		p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+		p.add(btnRent);
+		btnRent.setAlignmentX(CENTER_ALIGNMENT);
+		p.add(btnElectricity);
+		btnElectricity.setAlignmentX(CENTER_ALIGNMENT);
+		p.add(btnExpenditure);
+		btnExpenditure.setAlignmentX(CENTER_ALIGNMENT);
+		p.add(btnTrent);
+		btnTrent.setAlignmentX(CENTER_ALIGNMENT);
+		p.add(btntExpenditure);
+		btntExpenditure.setAlignmentX(CENTER_ALIGNMENT);
+		p.add(btnExit);
+		btnExit.setAlignmentX(CENTER_ALIGNMENT);
+		add(p);
+		setSize(400, 500);
+		setVisible(true);
+	}
 
-  if (e.getSource().equals(btnRent)) {
-   String input = JOptionPane.showInputDialog(this, "Enter Rent Amount");
-  if(gt.verify(input)) {
-   int rAmt = Integer.parseInt(input);
-   db.addRent(rAmt, "", "", true);
-   JOptionPane.showMessageDialog(this, " Rent Amount added successfully");
-  }
-  else {
-	  JOptionPane.showMessageDialog(this, " Please enter valid input");
-  }
-  } 
-  else if (e.getSource().equals(btnElectricity)) {
-   int firstRead = 0;
-   int secondRead = 0;
-   try {
-    int startRead = db.getAllRead().getFirstRead();
-    if (startRead <= 0) {
-     String input = JOptionPane.showInputDialog(this, "Enter the starting meter reading unit");
-     if(gt.verify(input)) {
-     firstRead = Integer.parseInt(input);
-     }
-     else
-     {
-    	 JOptionPane.showMessageDialog(this, " Please enter valid input");
-    	 
-     }
-    } 
-    else {
-     firstRead = db.getAllRead().getLastRead();
-    }
-    String input = JOptionPane.showInputDialog(this, "Enter the second meter reading unit");
-    if(gt.verify(input)) {
-    secondRead = Integer.parseInt(input);
-    }
-    else
-    {
-    	JOptionPane.showMessageDialog(this, " Please enter valid input");
-    }
-    db.addElectricity(firstRead, secondRead, "");
-    JOptionPane.showMessageDialog(this, "Electricity meter reading added successfully");
-   } catch (SQLException e1) {
-    // TODO Auto-generated catch block
-    e1.printStackTrace();
-    JOptionPane.showMessageDialog(null, e1.toString());
-   }
-  }
+	@Override
+	public void actionPerformed(ActionEvent e) {
 
-  else if (e.getSource().equals(btnExpenditure)) {
-   int itemPrice = 0;
-   try {
-   String itemName = JOptionPane.showInputDialog(this, "Enter the item name:");
-   
-   String inputPrice = JOptionPane.showInputDialog(this, "Enter the item price:");
-   if(gt.verify(inputPrice)) {
-   itemPrice = Integer.parseInt(inputPrice);
-   }
-   else
-   {
-	   JOptionPane.showMessageDialog(this, " Please enter valid input");
-   }
-   exp.setItem(itemName, itemPrice);   
-   db.addExpDaily(exp);
-   JOptionPane.showMessageDialog(this, "Expenditure meter added successfully");
-   } catch (SQLException e1) {
-    // TODO Auto-generated catch block
-    e1.printStackTrace();
-   }
-  }
-  else if (e.getSource().equals(btnTrent)) {
-   try {
-    int total = gt.getAllRead();
-    int rent = db.getRent();
-    int tRent = rent + (int)(total * unitPrice);
-    JOptionPane.showMessageDialog(this, " Total Rent Amount is: "+tRent);
-   } catch (SQLException e1) {
-    // TODO Auto-generated catch block
-    e1.printStackTrace();
-   }
+		if (e.getSource().equals(btnRent)) {
+			String input = "";
+			input = JOptionPane.showInputDialog(this, "Enter Rent Amount");
+			if (gt.verify(input)) {
+				int rAmt = Integer.parseInt(input);
+				db.addRent(rAmt, "", "", true);
+				JOptionPane.showMessageDialog(this, " Rent Amount added successfully");
+			} else {
+				JOptionPane.showMessageDialog(this, " Please enter valid input");
+			}
+		} else if (e.getSource().equals(btnElectricity)) {
+			int firstRead = 0;
+			int secondRead = 0;
+			try {
+				int startRead = db.getAllRead().getFirstRead();
+				if (startRead <= 0) {
+					String input = "";
+					input = JOptionPane.showInputDialog(this, "Enter the starting meter reading unit");
+					if (gt.verify(input)) {
+						firstRead = Integer.parseInt(input);
+					} else {
+						JOptionPane.showMessageDialog(this, " Please enter valid input");
 
-  }
-  else if (e.getSource().equals(btntExpenditure)) {
-	   try {
-	    int total = gt.TotalExpenses();
-	    JOptionPane.showMessageDialog(this, " Total Expenditure ofcurrent month is: "+total);
-	   } catch (SQLException e1) {
-	    // TODO Auto-generated catch block
-	    e1.printStackTrace();
-	   }
+					}
+				} else {
+					firstRead = db.getAllRead().getLastRead();
+				}
+				String input = JOptionPane.showInputDialog(this, "Enter the second meter reading unit");
+				if (gt.verify(input)) {
+					secondRead = Integer.parseInt(input);
+					db.addElectricity(firstRead, secondRead, "");
+					JOptionPane.showMessageDialog(this, "Electricity meter reading added successfully");
+				} else {
+					JOptionPane.showMessageDialog(this, " Please enter valid input");
+				}
 
-	  }
-  else if (e.getSource().equals(btnExit)) {
-	  System.exit(0);
-	  }
-  
- }
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+				JOptionPane.showMessageDialog(null, e1.toString());
+			}
+		}
 
- public static void main(String[] args) {
-  new MainGUI();
- }
+		else if (e.getSource().equals(btnExpenditure)) {
+			int itemPrice = 0;
+			try {
+				String itemName = JOptionPane.showInputDialog(this, "Enter the item name:");
+
+				String inputPrice = JOptionPane.showInputDialog(this, "Enter the item price:");
+				if (gt.verify(inputPrice)) {
+					itemPrice = Integer.parseInt(inputPrice);
+					exp.setItem(itemName, itemPrice);
+					db.addExpDaily(exp);
+					JOptionPane.showMessageDialog(this, "Expenditure meter added successfully");
+				} else {
+					JOptionPane.showMessageDialog(this, " Please enter valid input");
+				}
+
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		} else if (e.getSource().equals(btnTrent)) {
+			try {
+				int total = gt.getAllRead();
+				int rent = db.getRent();
+				int tRent = rent + (int) (total * unitPrice);
+				JOptionPane.showMessageDialog(this, " Total Rent Amount is: " + tRent);
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+		} else if (e.getSource().equals(btntExpenditure)) {
+			try {
+				int total = gt.TotalExpenses();
+				JOptionPane.showMessageDialog(this, " Total Expenditure ofcurrent month is: " + total);
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+		} else if (e.getSource().equals(btnExit)) {
+			System.exit(0);
+		}
+
+	}
+
+	public static void main(String[] args) {
+		new MainGUI();
+	}
 }
 
 class GrandTotal {
- int firstRead;
- int startRead;
- int lastRead;
- int unitPrice = 15;
- int totalUnit;
- int expenses;
- Expenditure expenditure;
- Electricity ele = new Electricity();
- Expenditure exp = new Expenditure();
+	int firstRead;
+	int startRead;
+	int lastRead;
+	int unitPrice = 15;
+	int totalUnit;
+	int expenses;
+	Expenditure expenditure;
+	Electricity ele = new Electricity();
+	Expenditure exp = new Expenditure();
 
- public int Total() {
-  firstRead = ele.getFirstRead();
-  lastRead = ele.getLastRead();
+	public int Total() {
+		firstRead = ele.getFirstRead();
+		lastRead = ele.getLastRead();
 
-  totalUnit = lastRead - firstRead;
-  int ebill = unitPrice * totalUnit;
-  return ebill;
- }
+		totalUnit = lastRead - firstRead;
+		int ebill = unitPrice * totalUnit;
+		return ebill;
+	}
 
- public int getAllRead() throws SQLException {
-  Database db = new Database();
-  int total = 0;
-  total = db.getAllRead().getLastRead() - db.getAllRead().getFirstRead();
-  return total;
- }
+	public int getAllRead() throws SQLException {
+		Database db = new Database();
+		int total = 0;
+		total = db.getAllRead().getLastRead() - db.getAllRead().getFirstRead();
+		return total;
+	}
 
- public int TotalExpenses() throws SQLException {
-  List < Expenditure > eList = new ArrayList < Expenditure > ();
-  Database db = new Database();
-  try {
-   eList = db.getAllExpenses();
-  } catch (SQLException e) {
-   // TODO Auto-generated catch block
-   e.printStackTrace();
-  }
-  int total = 0;
-  for (int i = 0; i < eList.size(); i++) {
-   total += eList.get(i).getItemPrice();
-  }
-  return total;
- }
- public boolean verify(String input) {
-     for(int i=0;i<input.length();i++)
-     {
-    	 if (Character.isDigit(input.charAt(i))== false)
-    	 {
-    		 return false; 
-    	 }
-    	
-     }
-     return true;
-}
+	public int TotalExpenses() throws SQLException {
+		List<Expenditure> eList = new ArrayList<Expenditure>();
+		Database db = new Database();
+		try {
+			eList = db.getAllExpenses();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		int total = 0;
+		for (int i = 0; i < eList.size(); i++) {
+			total += eList.get(i).getItemPrice();
+		}
+		return total;
+	}
+
+	public boolean verify(String input) {
+
+		if (input == null) {
+			return false;
+		}
+		for (int i = 0; i < input.length(); i++) {
+			if (Character.isDigit(input.charAt(i)) == false) {
+				return false;
+			}
+
+		}
+		return true;
+	}
 }
--- src/model/Rent.java
+++ src/model/Rent.java
@@ -1,32 +1,20 @@
 package model;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JTextField;
 
 public class Rent {//extends JFrame implements ActionListener {
 	private  int rent;
-//JFrame frame =new JFrame();
-	
-	public void Rent() {
+	public Rent() {
 		// TODO Auto-generated constructor stub
 	}
 
-//	public void setRent(JTextField text1) {
-//		JOptionPane.showMessageDialog(this, "correct");
-//	}
+public void setRent(int rent) {
+	this.rent=rent;
+		
+	}
 
 	public int getRent() {
 		return rent;
 	}
-
-	//@Override
-	//public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	//}
 
 }
 
